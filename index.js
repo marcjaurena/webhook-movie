db27590807888bf1cfeb77c1341e7a80d083f27c
Marc <marc@shawbe.com>
2019-05-21 15:16:35 +0200

    update

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const API_KEY = 'b94c0696';
-module.exports = API_KEY;
+'use strict';
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
-//const API_KEY = require('./apiKey');
+const API_KEY = require('./apiKey');
 
 const server = express();
 server.use(bodyParser.urlencoded({
@@ -16,7 +15,7 @@ server.use(bodyParser.json());
 server.post('/get-movie-details', (req, res) => {
 
     const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
-    const reqUrl = encodeURI(`http://www.omdbapi.com/?apikey=${API_KEY}&t=${movieToSearch}`);
+    const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
         responseFromAPI.on('data', (chunk) => {
@@ -24,7 +23,7 @@ server.post('/get-movie-details', (req, res) => {
         });
         responseFromAPI.on('end', () => {
             const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
+            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.` : '';
             dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
 
             return res.json({
--- listen.js
+++
@@ -1,3 +0,0 @@
-server.listen((process.env.PORT || 8000), () => {
-    console.log("Server is up and running...");
-});
